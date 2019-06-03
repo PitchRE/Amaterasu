a365237a7037a62e8816975cd17e60f4d9ecfdf2
PitchRE <pitch@redvelvet.pro>
2019-06-03 18:30:09 +0200

    items models, controllers and migrations.

---
+++ core/processing/database/factories/UserItemsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\User_items;
+use Faker\Generator as Faker;
+
+$factory->define(User_items::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
